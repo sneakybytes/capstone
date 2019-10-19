86b40e2a4e028cdc359d1fae0305da48bc8f93c6
Mohamed-Galal <mohamedg.ali20@gmail.com>
2019-10-19 04:06:25 +0200

    Edit kubectl configs

--- k8s_cluster.sh
+++ k8s_cluster.sh
@@ -4,8 +4,10 @@
 
 #sudo kubectl get all
 
-kubectl create -f ./kubernetes_config/k8s_deployment_config.yml
+#kubectl create -f ./kubernetes_config/k8s_deployment_config.yml
+#kubectl create -f ./kubernetes_config/k8s_service_config.yml
+#kubectl get all
 
-kubectl create -f ./kubernetes_config/k8s_service_config.yml
-
-kubectl get all
+dockerpath="sneakybytes/capstone-httpd:latest"
+kubectl run webapp --image=docker.io/$dockerpath --port=80 --generator=run-pod/v1
+kubectl expose pod/webapp --type=NodePort --port 80 --target-port 80
