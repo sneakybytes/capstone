78ec902279aa4b365f42a1b5aef5d740276195de
Mohamed-Galal <mohamedg.ali20@gmail.com>
2019-10-19 02:47:55 +0200

    Manual K8s cluster

--- k8s_cluster.sh
+++ k8s_cluster.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-sudo minikube start --vm-driver=none
+#sudo minikube start --vm-driver=none
 
-sudo kubectl get all
+#sudo kubectl get all
 
 sudo kubectl create -f ./kubernetes_config/k8s_deployment_config.yml
 
 sudo kubectl create -f ./kubernetes_config/k8s_service_config.yml
+
+sudo kubectl get all
